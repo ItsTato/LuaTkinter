add8851ce70987f0d241e4d95f6ff3815d8e7f09
ItsTato <78940659+ItsTato@users.noreply.github.com>
2024-05-04 04:02:08 -0600

    Add example program (Counter)

---
+++ Examples/counter.lua
@@ -0,0 +1,36 @@
+local MainWindow = new("Window");
+MainWindow.Name = "MainWindow";
+MainWindow.Title = "Counter App";
+MainWindow.Size = PxDim.new(240,80);
+
+local CountLabel = new("Label",MainWindow);
+CountLabel.Position = PxDim.new(10,10);
+
+local Add1Button = new("Button",MainWindow);
+Add1Button.Text = "Add 1 to count!";
+Add1Button.Position = PxDim.new(10,35);
+
+local Add100Button = new("Button",MainWindow);
+Add100Button.Text = "Add 100 to count!";
+Add100Button.Position = PxDim.new(110,35);
+
+local count = 0;
+CountLabel.Text = "Count is "..count;
+
+Add1Button:Bind(
+	"MouseButton1Click",
+	function()
+		count = count + 1;
+		CountLabel.Text = "Count is "..count;
+	end
+);
+
+Add100Button:Bind(
+	"MouseButton1Click",
+	function()
+		count = count + 100;
+		CountLabel.Text = "Count is "..count;
+	end
+);
+
+MainWindow:Start();
