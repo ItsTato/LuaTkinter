81074edf3f614a63cae48a935de5f8a0b8365369
ItsTato <78940659+ItsTato@users.noreply.github.com>
2024-05-04 15:23:44 -0600

    Update Binding system (Element base class owns every bind)

--- LuaTkinter/LuaTk/Element.py
+++ LuaTkinter/LuaTk/Element.py
@@ -3,6 +3,11 @@ from typing import Any, Callable
 class Element:
 	def __init__(self) -> None:
 		self.__name:str
+		self.__binds:dict[str,list[str]] = {
+			"MouseButton1Click": ["Button-1","Return"],
+			"MouseButton2Click": ["Button-3"],
+			"MouseScrollWheelClick": ["Button-2"]
+		}
 
 	@property
 	def Name(self) -> str:
@@ -12,18 +17,22 @@ class Element:
 		self.__name = new_name
 	
 	@property
+	def Binds(self) -> dict[str,list[str]]:
+		return self.__binds
+	@Binds.setter
+	def Binds(self,new_binds:dict[str,list[str]]) -> None:
+		self.__binds = new_binds
+	def addBind(self,new_bind:str,binds:list[str]) -> None:
+		self.__binds[new_bind] = binds
+	
+	@property
 	def TkCls(self) -> Any:
 		return
 	
 	def Bind(self,event:str,function:Callable) -> None:
 		if self.TkCls is None: raise Exception("Element is missing a TkCls property.")
-		match event:
-			case "MouseButton1Click":
-				self.TkCls.bind("<Button-1>",function)
-				self.TkCls.bind("<Return>",function)
-			case "MouseButton2Click":
-				self.TkCls.bind("<Button-3>",function)
-			case "MouseScrollWheelClick":
-				self.TkCls.bind("<Button-2>",function)
+		if not event in self.__binds: raise Exception("Elemenet has no such bind.")
+		for bind in self.__binds[event]:
+			self.TkCls.bind(f"<{bind}>",function)
 		return
 	
