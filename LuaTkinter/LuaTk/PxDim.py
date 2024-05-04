2f75de1262079eb1c8fb8dad1fcd61dcdb342b65
ItsTato <78940659+ItsTato@users.noreply.github.com>
2024-05-03 23:18:44 -0600

    Rename to PxDim

--- LuaTkinter/LuaTk/PxDim.py
+++ LuaTkinter/LuaTk/PxDim.py
@@ -1,26 +1,32 @@
-class PixDim:
-    def __init__(self,x:int,y:int) -> None:
-        self.__x:int = x
-        self.__y:int = y
-        return
-    
-    def new(x:int,y:int) -> "PixDim": # type: ignore
-        return PixDim(x,y)
-    
-    @property
-    def X(self) -> int:
-        return self.__x
-    @X.setter
-    def X(self,new_value:int) -> None:
-        self.__x = new_value
+class PxDim:
+	def __init__(self,x:int,y:int) -> None:
+		self.__x:int = x
+		self.__y:int = y
+		return
+	
+	def __str__(self) -> str: return f"{self.__x}x{self.__y}"
+	
+	def new(x:int,y:int) -> "PixDim": # type: ignore
+		return PxDim(x,y)
+	
+	@property
+	def X(self) -> int:
+		return self.__x
+	@X.setter
+	def X(self,new_value:int) -> None:
+		self.__x = new_value
 
-    @property
-    def Y(self) -> int:
-        return self.__y
-    @Y.setter
-    def Y(self,new_value:int) -> None:
-        self.__y = new_value
+	@property
+	def Y(self) -> int:
+		return self.__y
+	@Y.setter
+	def Y(self,new_value:int) -> None:
+		self.__y = new_value
 
-    @property
-    def Tuple(self) -> tuple:
-        return (self.__x,self.__y)
+	@property
+	def Tuple(self) -> tuple:
+		return (self.__x,self.__y)
+
+	@property
+	def String(self) -> str:
+		return self.__str__()
