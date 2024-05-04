b2c4df800eb823e0e62ac90705aadcf797a493b6
ItsTato <78940659+ItsTato@users.noreply.github.com>
2024-05-04 02:51:34 -0600

    Add Position property

--- LuaTkinter/LuaTk/Label.py
+++ LuaTkinter/LuaTk/Label.py
@@ -1,16 +1,50 @@
 from tkinter import ttk
-from typing import Any
 
 from .Element import Element
+from .PxDim import PxDim
+
+from ..Console import Message, Tags
 
 class Label(Element):
 	def __init__(self,parent:Element|None) -> None:
-		super().__init__(parent)
+		super().__init__()
 		self.Name = "Label"
-		self.__Tk = ttk.Label(parent.TkCls)
-		self.__Tk.config(text="WOW")
-		self.__Tk.pack()
+
+		self.__Tk = ttk.Label()
+
+		self.__parent:Element|None=parent
+		if parent is not None: self.Parent = parent
+		self.__text:str=""
+		self.__position:PxDim=PxDim(0,0)
+
+		self.Text = "Blank label"
 	
 	@property
-	def TkCls(self) -> Any: return self.__Tk
-	
+	def TkCls(self) -> ttk.Label: return self.__Tk
+
+	def __update_text(self) -> None: self.__Tk.config(text=self.__text)
+	def __update_position(self) -> None: self.__Tk.place(x=self.__position.X,y=self.__position.Y)
+
+	@property
+	def Parent(self) -> Element|None:
+		return self.__parent
+	@Parent.setter
+	def Parent(self,new_parent:Element|None) -> None:
+		self.__parent = new_parent
+		if isinstance(new_parent,Element): self.__Tk.master = new_parent.TkCls#; self.__Tk.pack()
+
+	@property
+	def Text(self) -> str:
+		return self.__text
+	@Text.setter
+	def Text(self,new_value:str) -> None:
+		self.__text = new_value
+		self.__update_text()
+
+	@property
+	def Position(self) -> PxDim:
+		return self.__position
+	@Position.setter
+	def Position(self,new_position:PxDim) -> None:
+		self.__position = new_position
+		self.__update_position()
--- testwindow.lua
+++ testwindow.lua
@@ -4,23 +4,25 @@ local MainWindow = new("Window");
 -- Window type elements cannot have a parent defined.
 
 -- Name the Window.
-MainWindow.Name = "MainWindow"
+MainWindow.Name = "MainWindow";
 
 -- Change the Window's title using the
 -- Title property of the Window class.
 MainWindow.Title = "Example Program";
 
---[[ Change window's geometry to a 100x100 box.
-MainWindow.Size = PxDim.new(400,300);     Default is 400x300
---]]
+---- Change window's geometry to a 100x100 box.
+-- MainWindow.Size = PxDim.new(400,300);     Default is 400x300
 
---[[ Alter max size of window to being 300x300.
-MainWindow.MaxSize = PxDim.new(300,300);  Default is no max.
---]]
+---- Alter max size of window to being 300x300.
+-- MainWindow.MaxSize = PxDim.new(300,300);  Default is no max.
 
 local HelloLabel = new("Label",MainWindow);
 
+HelloLabel.Text = "Woah! Amazing...";
+
+HelloLabel.Position = PxDim.new(10,10);
+
 -- Starts the MainWindow loop.
+MainWindow.run();
 -- No code can be ran after this is called,
 -- assign anything you need before this.
-MainWindow.run();
